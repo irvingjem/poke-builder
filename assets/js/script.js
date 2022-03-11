ad23492415fadce59a0a37069ea6a64787a92b04
Jem Irving <irving.jem@gmail.com>
2022-03-10 20:42:40 -0800

    created skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,23 @@
-var apiNames = ""
-fetch();
+var apiNames = "https://randomuser.me/api/"
+fetch(
+        apiNames
+    )
+    .then(function(nameResponse) {
+        return cityResponse.json();
+    });
+
+var apiRaces = "https://www.dnd5eapi.co/api/races"
+fetch(
+        apiRaces
+    )
+    .then(function(raceResponse) {
+        return raceResponse.json();
+    });
+
+var apiClasses = "https://www.dnd5eapi.co/api/classes"
+fetch(
+        apiClasses
+    )
+    .then(function(classResponse) {
+        return classResponse.json();
+    });
