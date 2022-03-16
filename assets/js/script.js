8364bfc7beaa9fc32213d3a77c1eee99a64e993e
KingAusti <austin@thehenrys.xyz>
2022-03-15 20:53:28 -0700

    1st try grab and append pokemon name

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,43 @@ fetch(
         return namesResponse.json();
     });
 
-// Display Name Function
-
-
+// Display Name Function - Youre doing this one Austin
+let pokeDiv = document.ElementById("pokename");
+let getPokeName = function() {
+    pokeDiv.classList.add("font-bold", "text-xl", "mb-2");
+    pokeDiv.innerHTML = 'Name: <p id="pokename"></p>';
+};
+//append html to display name
+pokeDiv.append(getPokeName);
+//End of Austin work
+//Austin note
+    /*
+    var hours = [“09”, “10", “11”, “12", “13”, “14", “15”, “16", “17”];
+    // for each of the hours listed above
+    // creates a div with classes and inner html generated for each hour
+    // template literals ${hour} apply to each row
+    
+    let containerEl = document.ElementById("pokename")
+    hours.forEach(function (hour) {
+      
+    var pokeDiv = document.createElement(“div”);
+    let getPokeName = function() {
+        pokeDiv.classList.add(“font-bold”, “text-xl”, "mb-2");
+        pokeDiv.innerHTML = `Name: <p id="pokename"></p>'
+    }
+      <textarea class=“col-md-10 description ${timeColor(hour)}“>${
+        localStorage.getItem(hour) || “”
+      
+      
+      </textarea>
+      <button data-hour=“${hour}” class=“btn saveBtn col-md-1” onclick=“saveText(this)“>
+        <i class=“fas fa-save”></i>
+      </button>`;
+      // appends to container
+      pokeDiv.append(getPokeName);
+    }); 
+    */
+//End Austin note
 // Fetch Abilities Function 
 
 var apiStats = "https://pokeapi.co/"
