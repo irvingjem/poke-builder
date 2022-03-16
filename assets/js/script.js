b4472ca9e875914b731126c4ff87fd113c6efa9e
superfishal <97133716+superfishal@users.noreply.github.com>
2022-03-15 21:30:33 -0700

    built ability display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,34 @@
-// Fetch Name Function 
+// Fetch Name Function
 
-
-var apiNames = "https://fungenerators.com/name/pokemon/"
-fetch(
-        apiNames
-    )
-    .then(function(namesResponse) {
-        return namesResponse.json();
-    });
+var apiNames = "https://fungenerators.com/name/pokemon/";
+fetch(apiNames).then(function (namesResponse) {
+  return namesResponse.json();
+});
 
 // Display Name Function
 
+// Fetch Abilities Function
 
-// Fetch Abilities Function 
-
-var apiStats = "https://pokeapi.co/"
-fetch(
-        apiStats
-    )
-    .then(function(statsResponse) {
-        return statsResponse.json();
-    });
+var apiStats = "https://pokeapi.co/";
+fetch(apiStats).then(function (statsResponse) {
+  return statsResponse.json();
+});
 
 // Display Abilities function
 
-
+var abilitiesList = document.getElementById("abilities");
+var moves = ["Static", "Lightning Rod"];
+// for each of the moves listed above
+// creates a div with classes and inner html generated for each move
+// template literals ${move} apply to each row
+moves.forEach(function (move) {
+  var moveListEl = document.createElement("li");
+  //   moveListEl.classList.add("text-gray-l");
+  moveListEl.innerHTML = `${move}`;
+  // appends to container
+  abilitiesList.append(moveListEl);
+  console.log(this);
+});
 
 // Event listener for Generate, Save, Delete
 
@@ -33,7 +37,7 @@ fetch(
 //     // get city name value from input element
 //     var cityname = cityNameInputEl.value.trim();
 
-//     // Set city name in local storage and generate search history 
+//     // Set city name in local storage and generate search history
 //     if (cityname) {
 //         searchHistoryArray.push(cityname);
 //         localStorage.setItem("weatherSearch", JSON.stringify(searchHistoryArray));
@@ -51,25 +55,15 @@ fetch(
 
 // }
 
-
-
-
-
-
 // Function to grab local image (for now)
 
 // Look in assets folder append to child probably div and create the img
 
-
-
 // Save local | load local (last priority) | Delete Local
 
-
-var loadHistory = function() {
-    searchArrray =
-}
-
-
+// var loadHistory = function() {
+//     searchArrray =
+// }
 
 // var loadHistory = function() {
 //     searchArray = JSON.parse(localStorage.getItem("weatherSearch"));
@@ -87,6 +81,5 @@ var loadHistory = function() {
 
 //     }
 // }
-
 
 //loadHistory();
