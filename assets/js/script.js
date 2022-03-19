a45fea1a44b94de00946ab393dc2a5068b5b4e3f
superfishal <97133716+superfishal@users.noreply.github.com>
2022-03-18 17:40:57 -0700

    button/input/html tailwind styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,13 @@ var fetchPokeData = function (pokeName) {
       return pokeNameResponse.json();
     })
     .then(function (pokeNameData) {
-      console.log("pokeNameData", pokeNameData);
+      console.log("pokeNameData", pokeNameData.name);
       resetScreen();
 
       //setting up the character name
       var dataTypes = pokeNameData["name"];
-      document.getElementById("pokename").textContent = "Name: " + dataTypes;
-
+      document.getElementById("pokename").textContent =
+        "Name: " + dataTypes[0].toUpperCase() + dataTypes.substring(1);
       //Calling the ability fetch based on the character name data
       fetchPokeAbility(dataTypes);
 
@@ -32,50 +32,22 @@ function resetScreen() {
   abilities.innerHTML = "";
 }
 
-// Display Name Function - Youre doing this one Austin // I updated this to make sure it was printing correctly
+// Display Name Function -
 let pokeDiv = document.getElementById("pokeDetails");
 let getPokeName = function () {
   pokeDiv.classList.add("font-bold", "text-xl", "mb-2");
   var pTag = document.createElement("p");
   pTag.setAttribute("id", "pokename");
   pTag.textContent = "Name: ";
+
   console.log(pTag);
   //pokeDiv.innerHTML = 'Name: <p id="pokename"></p>';
   //append html to display name
+
   pokeDiv.append(pTag);
 };
 
-//End of Austin work
-getPokeName(); //
-
-//Austin note
-/*
-var hours = [“09”, “10", “11”, “12", “13”, “14", “15”, “16", “17”];
-// for each of the hours listed above
-// creates a div with classes and inner html generated for each hour
-// template literals ${hour} apply to each row
-    
-let containerEl = document.ElementById("pokename")
-hours.forEach(function (hour) {
-  
-var pokeDiv = document.createElement(“div”);
-let getPokeName = function() {
-    pokeDiv.classList.add(“font-bold”, “text-xl”, "mb-2");
-    pokeDiv.innerHTML = `Name: <p id="pokename"></p>'
-}
-  <textarea class=“col-md-10 description ${timeColor(hour)}“>${
-    localStorage.getItem(hour) || “”
-  
-  
-  </textarea>
-  <button data-hour=“${hour}” class=“btn saveBtn col-md-1” onclick=“saveText(this)“>
-    <i class=“fas fa-save”></i>
-  </button>`;
-  // appends to container
-  pokeDiv.append(getPokeName);
-}); 
-*/
-//End Austin note
+getPokeName();
 
 // Fetch Abilities Function -- Jem
 
@@ -86,104 +58,41 @@ var fetchPokeAbility = function (pokeName) {
       return pokeAbilityResponse.json();
     })
     .then(function (pokeAbilityData) {
-      console.log("pokeAbilityData", pokeAbilityData);
       //resetScreen();
 
       var dataAbility = pokeAbilityData.abilities;
+      console.log("pokeAbilityData", pokeAbilityData.abilities);
       var dataFirstAbility = dataAbility[0];
       var dataSecondAbility = dataAbility[1];
       var dataThirdAbility = dataAbility[2];
       console.log("first", dataFirstAbility, "second", dataSecondAbility);
       //createElement
       var liTag = document.createElement("li");
-      liTag.textContent = dataFirstAbility.ability.name;
+      liTag.textContent =
+        // a lot of code to capitalize the first letter
+        dataFirstAbility.ability.name.charAt(0).toUpperCase() +
+        dataFirstAbility.ability.name.slice(1);
       //append it to ul List
       abilitiesList.append(liTag);
       //createElement
       var liTag2 = document.createElement("li");
-      liTag2.textContent = dataSecondAbility.ability.name;
+      liTag2.textContent =
+        // a lot of code to capitalize the first letter
+        dataSecondAbility.ability.name.charAt(0).toUpperCase() +
+        dataSecondAbility.ability.name.slice(1);
       //append it to ul List
       abilitiesList.append(liTag2);
       //createElement
       var liTag3 = document.createElement("li");
-      liTag3.textContent = dataThirdAbility.ability.name;
+      liTag3.textContent =
+        // a lot of code to capitalize the first letter
+        dataThirdAbility.ability.name.charAt(0).toUpperCase() +
+        dataThirdAbility.ability.name.slice(1);
       //append it to ul List
-      abilitiesList.append(liTag3);
-      /*
-            pokeAbilityOne.textContent = capitalize(dataFirstAbility['ability']);
-            if (dataSecondType) {
-                pokeAbilityTwo.classList.remove('hide');
-                pokeAbilityTwo.textContent = capitalize(dataSecondAbility['ability']);
-            } else {
-                pokeAbilityTwo.classList.add('hide');
-                pokeAbilityTwo.textContent = '';
-            }
-            mainScreen.classList.add(dataFirstType['abilities']);
 
-            pokeName.textContent = capitalize(data['abilities']);
-            pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
-            pokeFrontImage.src = data['sprites']['front_default'] || '';
-            pokeBackImage.src = data['sprites']['back_default'] || '';
-            */
+      abilitiesList.append(liTag3);
     });
 };
-
-// Display Abilities function -- Andy
-
-// var moves = ["Static", "Lightning Rod"];
-// for each of the moves listed above
-// creates a div with classes and inner html generated for each move
-// template literals ${move} apply to each row
-// moves.forEach(function(move) {
-//     var moveListEl = document.createElement("li");
-//     //   moveListEl.classList.add("text-gray-l");
-//     moveListEl.innerHTML = `${move}`;
-//     // appends to container
-//     abilitiesList.append(moveListEl);
-//     console.log(this);
-// });
-
-// Event listener for Generate, Save, Delete -- TBD
-
-/* <button id="rounded-md shadow">Generate</button>
-document.getElementById("mybtn").addEventListener("click", function() {
-    
-})
-
-element.addEventListener("click", buttonClickhandler);
-function buttonClickhandler(event) {
-    console.log('Button Clicked');
-}
-const btn= document.querySelector('.btn');
-btn.addEventListener('click', buttonClickhandler); */
-//}
-
-// var formSubmitHandler = function(event) {
-//     event.preventDefault();
-//     // get city name value from input element
-//     var cityname = cityNameInputEl.value.trim();
-
-//     // Set city name in local storage and generate search history
-//     if (cityname) {
-//         searchHistoryArray.push(cityname);
-//         localStorage.setItem("weatherSearch", JSON.stringify(searchHistoryArray));
-//         var searchHistoryEl = document.createElement('button');
-//         searchHistoryEl.className = "btn";
-//         searchHistoryEl.setAttribute("data-city", cityname)
-//         searchHistoryEl.innerHTML = cityname;
-//         historyButtonsEl.appendChild(searchHistoryEl);
-//         historyCardEl.removeAttribute("style")
-//         getWeatherInfo(cityname);
-//         cityNameInputEl.value = "";
-//     } else {
-//         alert("Please enter a City name");
-//     }
-
-// }
-
-// Function to grab local image (for now)
-
-// Look in assets folder append to child probably div and create the img
 
 // Save local | load local (last priority) | Delete Local
 
